124fb42a42a5659ed66da82175816ec20203a375
Mert <mertparlak123@hotmail.com>
2024-10-06 13:29:27 +0300

    Added phone validation use with react-router error handler. (useActionData)

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect, useNavigation } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -36,6 +36,7 @@ function CreateOrder() {
   const cart = fakeCart;
   const navigation = useNavigation();
   const isLoading = navigation.state === "submitting";
+  const errors = useActionData();
 
   return (
     <div>
@@ -51,6 +52,7 @@ function CreateOrder() {
           <label>Phone number</label>
           <div>
             <input type="tel" name="phone" required />
+            {errors?.phone && <p>{errors.phone}</p>}
           </div>
         </div>
 
@@ -91,6 +93,16 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === "on",
   };
+
+  let errors = {};
+
+  if (!isValidPhone(customData.phone)) {
+    errors.phone = "Invalid phone number";
+  }
+
+  if (Object.keys(errors).length) {
+    return errors;
+  }
 
   const newOrder = await createOrder(customData);
 
