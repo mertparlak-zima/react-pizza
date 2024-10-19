162d095c4e5efb5392c3253526ab50fe9a6c7ceb
Mert <mertparlak123@hotmail.com>
2024-10-19 16:35:21 +0300

    Refactor Username component to handle empty name field

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -3,5 +3,9 @@ import { useSelector } from "react-redux";
 export default function Username() {
   const { name } = useSelector((store) => store.user);
 
+  if (!name) {
+    return null;
+  }
+
   return <div className="hidden text-sm font-semibold md:block">{name}</div>;
 }
