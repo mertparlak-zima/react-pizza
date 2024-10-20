a2a0866100424bb439b34673d0f4ad09daa58ed3
Mert <mertparlak123@hotmail.com>
2024-10-20 16:54:07 +0300

    Refactor Home component to conditionally render CreateUser or Continue Ordering button based on user name

--- src/features/ui/Home.jsx
+++ src/features/ui/Home.jsx
@@ -1,6 +1,10 @@
 import CreateUser from "../user/CreateUser";
+import { useSelector } from "react-redux";
+import Button from "./Button";
 
 function Home() {
+  const { name } = useSelector((store) => store.user);
+
   return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="mb-8 text-center text-xl font-semibold md:text-3xl">
@@ -9,7 +13,15 @@ function Home() {
         <span className="text-yellow-500">
           Straight out of the oven, straight to you.
         </span>
-        <CreateUser />
+        {name === "" ? (
+          <CreateUser />
+        ) : (
+          <div className="mt-6">
+            <Button to="/menu" type="primary">
+              Continue irderung, {name}
+            </Button>
+          </div>
+        )}
       </h1>
     </div>
   );
